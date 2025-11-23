56e023dd3f6d1f60a5728a66e2c9732a94a721bf
simon minami <simon.sho.minami@gmail.com>
2025-11-23 11:47:03 -0500

    indent

--- muzero/config.py
+++ muzero/config.py
@@ -1,47 +1,47 @@
 class MuZeroConfig(object):
 
-  def __init__(self):
-    
-    ### Need to tweak these parameters
-    self.action_space_size = 15 #number of actions we want to control (discrete steering and throttle)
+	def __init__(self):
+		
+		### Need to tweak these parameters
+		self.action_space_size = 15 #number of actions we want to control (discrete steering and throttle)
 
-    # will yeild about 30sec per episode if you step through the env at 10Hz (Probably increase it after initial training)
-    self.max_moves = 300 #max environment steps per episode
+		# will yeild about 30sec per episode if you step through the env at 10Hz (Probably increase it after initial training)
+		self.max_moves = 300 #max environment steps per episode
 
-    self.num_simulations = 50 #number of simulations MCTS runs per decision step
-    self.td_steps = 10 # num of steps in the future to boot strap (Would be inf for monte carlo)
-    self.discount = 0.99 #typical discount rate for future rewards
+		self.num_simulations = 50 #number of simulations MCTS runs per decision step
+		self.td_steps = 10 # num of steps in the future to boot strap (Would be inf for monte carlo)
+		self.discount = 0.99 #typical discount rate for future rewards
 
-    #encourages exploration at the root of the search tree
-    self.dirichlet_alpha = 0.3
-    self.root_exploration_fraction = 0.25 
+		#encourages exploration at the root of the search tree
+		self.dirichlet_alpha = 0.3
+		self.root_exploration_fraction = 0.25 
 
-    # UCB formula - Upper Confidence Bound
-    # An algorithm used to solve and manage the fundamental 
-    # exploration-exploitation trade-off
-    self.pb_c_base = 19652
-    self.pb_c_init = 1.25
+		# UCB formula - Upper Confidence Bound
+		# An algorithm used to solve and manage the fundamental 
+		# exploration-exploitation trade-off
+		self.pb_c_base = 19652
+		self.pb_c_init = 1.25
 
-    # If we already have some information about which values occur in the
-    # environment, we can use them to initialize the rescaling.
-    self.known_bounds = None #Can change this if 
+		# If we already have some information about which values occur in the
+		# environment, we can use them to initialize the rescaling.
+		self.known_bounds = None #Can change this if 
 
-    ### Training
-    self.training_steps = 50000
-    self.checkpoint_interval = int(1e3)
-    self.window_size = int(1e5) # replay buffer size (max number of experience you store)
-    self.batch_size = 32 #batch size for trainig
-    self.num_unroll_steps = 5 # future steps the dynamics model is trained to model at once
+		### Training
+		self.training_steps = 50000
+		self.checkpoint_interval = int(1e3)
+		self.window_size = int(1e5) # replay buffer size (max number of experience you store)
+		self.batch_size = 32 #batch size for trainig
+		self.num_unroll_steps = 5 # future steps the dynamics model is trained to model at once
 
-    self.weight_decay = 0.0  #prevents the weights from blowing up or overfitting (can set it to 1e-4 if needed)
-    self.momentum = 0.9
+		self.weight_decay = 0.0  #prevents the weights from blowing up or overfitting (can set it to 1e-4 if needed)
+		self.momentum = 0.9
 
-    # Exponential learning rate scheduler
-    self.lr_init = 1e-3
-    self.lr_decay_rate = 0.1
-    self.lr_decay_steps = 2000
+		# Exponential learning rate scheduler
+		self.lr_init = 1e-3
+		self.lr_decay_rate = 0.1
+		self.lr_decay_steps = 2000
 
-  
-  def visit_softmax_temperature(self, num_moves, training_steps):
-    #Determines the exploration rate and exploitation based on what training step we are on
-    return 1.0 if training_steps < 10000 else 0.25
+	
+	def visit_softmax_temperature(self, num_moves, training_steps):
+		#Determines the exploration rate and exploitation based on what training step we are on
+		return 1.0 if training_steps < 10000 else 0.25
--- train_muzero.py
+++ train_muzero.py
@@ -4,22 +4,22 @@ from muzero.mcts import Node, run_mcts, expand_node, add_exploration_noise, sele
 from muzero.game import Game, DrivingEnvironment
 
 def play_game(config, network, env):
-    game = Game(config, env)
+	game = Game(config, env)
 
-    while not game.terminal():
-        root = Node(0)
-        current_obs = game.make_image(-1)          # occupancy grid
-        network_output = network.initial_inference(current_obs)
-        expand_node(root, game.to_play(), game.legal_actions(), network_output)
-        add_exploration_noise(config, root)
+	while not game.terminal():
+		root = Node(0)
+		current_obs = game.make_image(-1)          # occupancy grid
+		network_output = network.initial_inference(current_obs)
+		expand_node(root, game.to_play(), game.legal_actions(), network_output)
+		add_exploration_noise(config, root)
 
-        # MCTS
-        run_mcts(config, root, game.action_history(), network)
+		# MCTS
+		run_mcts(config, root, game.action_history(), network)
 
-        # Choose action
-        action = select_action(config, len(game.history), root, network)
+		# Choose action
+		action = select_action(config, len(game.history), root, network)
 
-        game.apply(action)
-        game.store_search_statistics(root)
+		game.apply(action)
+		game.store_search_statistics(root)
 
-    return game
+	return game
